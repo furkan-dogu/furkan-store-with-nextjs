abbe5e124cccb8d95f2a6c5134b408c5b7e85e3f
Furkan Doğu <furkandogu2018@gmail.com>
2024-05-10 20:53:56 +0300

    düzenleme

--- hooks/useProductCalls.jsx
+++ hooks/useProductCalls.jsx
@@ -4,13 +4,13 @@ import { toastErrorNotify } from "@/helpers/ToastNotify";
 import { fetchFail, fetchStart, getProductsSuccess } from "@/redux/features/productSlice";
 
 const useProductCalls = () => {
-    const { axiosWithToken } = useAxios();
+    const { axiosPublic } = useAxios();
     const dispatch = useDispatch();
 
     const getProducts = async (search) => {
         dispatch(fetchStart());
         try {
-            const { data } = await axiosWithToken(`/products?limit=100&search=${search}`);
+            const { data } = await axiosPublic(`/products?limit=100&search=${search}`);
             dispatch(getProductsSuccess(data))
         } catch (error) {
             dispatch(fetchFail());
