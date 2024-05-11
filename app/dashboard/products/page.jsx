ebac9033c2d4dcde6ddff24c870e9279be82f0f6
Furkan Doğu <furkandogu2018@gmail.com>
2024-05-11 20:24:29 +0300

    düzenleme

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -41,7 +41,7 @@ const Products = () => {
     <div className="product-container">
       <div className="mb-8 flex justify-center items-center">
         <select
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 uppercase cursor-pointer"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 uppercase cursor-pointer"
           onChange={handleChange}
         >
           {categoryArr.map((item, index) => (
